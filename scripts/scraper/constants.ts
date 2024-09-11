a468433d16303f8319028f724b9941040f644cfb
Andy Blum <andy.blum.01@gmail.com>
2024-09-10 22:41:55 -0400

    Fix week projection scraping

--- scripts/scraper/constants.ts
+++ scripts/scraper/constants.ts
@@ -16,7 +16,7 @@ export const fantasyApi = {
     headers: {
       'Content-Type': 'application/json',
       'Cookie': `espn_s2=${ESPN_S2}; SWID=${SWID};`,
-      'X-Fantasy-Filter': `{"players":{"filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,23,24]},"filterRanksForScoringPeriodIds":{"value":[1]},"limit":3000,"offset":0,"sortPercOwned":{"sortAsc":false,"sortPriority":1},"sortDraftRanks":{"sortPriority":100,"sortAsc":true,"value":"STANDARD"},"filterRanksForRankTypes":{"value":["PPR"]},"filterRanksForSlotIds":{"value":[0,2,4,6,17,16,8,9,10,12,13,24,11,14,15]},"filterStatsForTopScoringPeriodIds":{"value":2,"additionalValue":["002024","102024","002023","1120241","022024"]}}}`
+      'X-Fantasy-Filter': `{"players":{"filterSlotIds":{"value":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,23,24]},"filterRanksForScoringPeriodIds":{"value":[1]},"limit":3000,"offset":0,"sortPercOwned":{"sortAsc":false,"sortPriority":1},"sortDraftRanks":{"sortPriority":100,"sortAsc":true,"value":"STANDARD"},"filterRanksForRankTypes":{"value":["PPR"]},"filterRanksForSlotIds":{"value":[0,2,4,6,17,16,8,9,10,12,13,24,11,14,15]}}}`
     },
   },
 }
