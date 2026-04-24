0ec9e14caa6e2b5fa6c5222bb334c171ad89e7aa
Andy Blum <andy.blum.01@gmail.com>
2026-04-24 13:31:50 -0400

    throw errors in mail util for more visibility

--- utils/mail.ts
+++ utils/mail.ts
@@ -1,14 +1,12 @@
 import nodemailer from "nodemailer";
 import 'dotenv/config';
 
-const {
-  GPASS
-} = process.env;
+const { GPASS } = process.env;
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false, // use false for STARTTLS; true for SSL on port 465
+  secure: false,
   auth: {
     user: 'thelogffb@gmail.com',
     pass: GPASS,
@@ -16,15 +14,17 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendMail(recipient: string, subject: string, message: string) {
-  await transporter.sendMail({
-    from: '"The Commish" <thelogffb@gmail.com>', // sender address
-    to: recipient,
-    subject,
-    html: `<b>Message follows:</b> ${message}`,
-  }).then(info => {
-    console.log("Message sent.");
-    console.dir(info);
-  }).catch(console.error)
+  try {
+    await transporter.sendMail({
+      from: '"The Commish" <thelogffb@gmail.com>',
+      to: recipient,
+      subject,
+      html: `<b>Message follows:</b> ${message}`,
+    });
+  } catch (err) {
+    console.error('mail send failed', { recipient, subject, err });
+    throw err;
+  }
 }
 
 export default sendMail;
